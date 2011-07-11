77c31bd177c16a45e5a69bbaf1e0ba1503ddc248
Dave Waller <dave.waller@mkmlabs.com>
2011-07-11 16:19:41 +0100

    Commit of missing config file

---
+++ .config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**************************************************************/
+/* MKMLabs™ InteropAbility Framework Builder [prototype]      */
+/* Created by Dave Waller © MKMLabs™ 2011                     */
+/**************************************************************/
+
+$config = array(
+	"db" => array(
+		"dbname" => "abilitybuilder",
+		"username" => "root",
+		"password" => "root",
+		"host" => "127.0.0.1"
+	)
+);
+
+ini_set("error_reporting", "true");
+error_reporting(E_ALL|E_STRCT);
+
+?>
